e4d9b83adb1a01b52472cb756e257a778bca7272
stanleyhuang1 <stanley.huang22@sitechhs.com>
2021-05-14 17:42:33 -0400

    s

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://dog.ceo/api/breeds/image/random
+      `
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    alert("Hey, something went wrong!");
+  }
+};
